9247e165c4d977472076745cdf2d201ac999886a
sivaji14325 <sivajiofficial80@gmail.com>
2025-01-09 11:36:26 +0530

    done

--- app1/admin.py
+++ app1/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Employee
 # Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    emp_data = ['eid','ename','esal']
+admin.site.register(Employee,EmployeeAdmin)
--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Employee(models.Model):
+    eid  = models.IntegerField(primary_key=True)
+    ename = models.CharField(max_length=40)
+    esal = models.FloatField(default=None)
+    def __str__(self):
+        return self.ename
