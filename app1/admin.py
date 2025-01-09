caa0185ac1691c2945a8caea9c70e8a9d67d0f1d
sivaji14325 <sivajiofficial80@gmail.com>
2025-01-09 12:00:40 +0530

    done2

--- app1/admin.py
+++ app1/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Employee
+from app1.models import Employee
 # Register your models here.
 class EmployeeAdmin(admin.ModelAdmin):
     emp_data = ['eid','ename','esal']
--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from app1.serializer import EmployeeSerializers
+from app1.models import Employee
+from rest_framework.response import Response
+from rest_framework.views import APIView
 # Create your views here.
+class EmpView(APIView):
+    def get(self,request):
+        instance=Employee.objects.all()
+        serializer = EmployeeSerializers(instance,many=True)
+        return Response(serializer.data)
